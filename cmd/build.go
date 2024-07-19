4b19ff87fcd4fe34a38e418fd2cb98c969de2109
djj <shuaiqijianhao@qq.com>
2024-07-20 01:00:03 +0800

    Refactor: Simplify and remove unnecessary comments in cmd/build.go and cmd/remove.go
    - Removed redundant comments and flags setup in cmd/build.go.
    - Cleaned up the header comments in cmd/remove.go.

--- cmd/build.go
+++ cmd/build.go
@@ -16,14 +16,4 @@ var buildCmd = &cobra.Command{
 
 func init() {
 	repoCmd.AddCommand(buildCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// buildCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// buildCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
